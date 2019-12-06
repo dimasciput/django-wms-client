016649a0c371fcfa4a400542c1992b9354a31997
Dimas Ciputra <dimas.ciputra@gmail.com>
2019-12-06 19:10:00 +0700

    Update urls.py

--- wms_client/urls.py
+++ wms_client/urls.py
@@ -2,9 +2,10 @@
 """URI Routing configuration for this apps."""
 from django.conf.urls import url
 from mezzanine.conf import settings
+from .views import map
 
 _slash = "/" if settings.APPEND_SLASH else ""
 
 urlpatterns = [
-    url(r"^(?P<slug>.*)%s$" % _slash, 'wms_client.views.map', name='wms_map'),
+    url(r"^(?P<slug>.*)%s$" % _slash, map, name='wms_map'),
 ]
